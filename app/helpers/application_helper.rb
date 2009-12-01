e951e0d5236b9a4c67fdcd4a78dd84105a7a5b3e
Justin Marney <gotascii@gmail.com>
2009-12-01 10:48:48 -0500

    card links open in the same different window

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def card_columns(card)
-    html = content_tag :td, link_to(card.titleized_name, card.url)
+    html = content_tag :td, link_to(card.titleized_name, card.url, :target => "_mtg")
     html += [card.color.name, card.expansion.abbr].collect do |field|
       content_tag :td, field
     end.join
