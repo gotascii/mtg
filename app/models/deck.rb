0b7aa4ccae295c1d561f100bc6b770238ae0d6bf
Justin Marney <gotascii@gmail.com>
2009-12-01 11:59:46 -0500

    added total card output to deck/show

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,6 +3,10 @@ class Deck < ActiveRecord::Base
   validates_presence_of :name
   named_scope :order_by_name, :order => :name
 
+  def total
+    picks.collect(&:total).sum
+  end
+
   def wanted_picks
     picks.select{|p| p.diff > 0 }
   end
