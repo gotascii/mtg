121158a7411bb4456088dde9afb784bee5056e87
Justin Marney <gotascii@gmail.com>
2009-12-01 23:11:44 -0500

    deck name editing and deleting

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,7 @@
 class DecksController < ApplicationController
   before_filter :load_colors_with_cards, :only => [:show]
+  before_filter :load_deck, :only => [:show, :destroy, :edit, :update]
+  before_filter :load_decks, :only => [:index, :update]
 
   def index
     @deck = Deck.new
@@ -7,7 +9,6 @@ class DecksController < ApplicationController
   end
 
   def show
-    @deck = Deck.find(params[:id])
     @picks = @deck.picks
     @pick = Pick.new(:deck => @deck)
   end
@@ -15,5 +16,26 @@ class DecksController < ApplicationController
   def create
     @deck = Deck.create!(params[:deck])
     redirect_to decks_url
+  end
+
+  def destroy
+    @deck.delete
+    redirect_to decks_url
+  end
+
+  def edit
+  end
+
+  def update
+    @deck.update_attributes(params[:deck])
+  end
+
+  private
+  def load_deck
+    @deck = Deck.find(params[:id])
+  end
+
+  def load_decks
+    @decks = Deck.order_by_name
   end
 end
