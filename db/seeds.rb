8c565df15b93c765eced52e2cc813864e61b9f00
Justin Marney <gotascii@gmail.com>
2009-12-04 11:23:41 -0500

    update expansion abbr to official codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,17 @@
 end
 
 [["M2010", "M10", "Magic_2010_Common.gif"],
-["Zendikar", "ZK", "Zendikar_Common.gif"],
-["Conflux", "CF", "Conflux_Common.gif"],
-["9th Core", "IX", "Exp_sym_9th.png"],
-["10th Ed", "X", "Exp_sym_10th.png"],
-["Fallen Empires", "FE", "Exp_sym_fallenempires.png"],
-["Shards of Alara", "SA", "20px-Shards_of_Alara.png"],
-["Alara Reborn", "AR", "Alara_Reborn_Common.gif"],
-["Shadowmoor", "SM", "21px-Exp_sym_shadowmoor.png"],
-["Morningtide", "MT", "20px-Exp_sym_morningtide.png"]].each do |name, abbr, icon|
-  e = Expansion.find_or_create_by_name_and_abbr(name, abbr)
+["Zendikar", "ZEN", "Zendikar_Common.gif"],
+["Conflux", "CON", "Conflux_Common.gif"],
+["9th Core", "9ED", "Exp_sym_9th.png"],
+["10th Ed", "10E", "Exp_sym_10th.png"],
+["Fallen Empires", "FEM", "Exp_sym_fallenempires.png"],
+["Shards of Alara", "ALA", "20px-Shards_of_Alara.png"],
+["Alara Reborn", "ARB", "Alara_Reborn_Common.gif"],
+["Shadowmoor", "SHM", "21px-Exp_sym_shadowmoor.png"],
+["Morningtide", "MOR", "20px-Exp_sym_morningtide.png"]].each do |name, abbr, icon|
+  e = Expansion.find_or_create_by_name(name)
+  e.abbr = abbr
   e.icon = icon
   e.save
 end
